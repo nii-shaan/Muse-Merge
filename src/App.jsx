9c4d8d8b7d8374406352b21acc4793fefda7d1cd
Nishan Bista <nishanbista525@gmail.com>
2024-05-04 01:04:40 +0545

    signup page done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { useEffect } from "react";
 import { login, logout } from "./store/slices/Auth";
 import { Header, Footer } from "./components/index";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const dispatch = useDispatch();
@@ -28,6 +30,7 @@ function App() {
     return (
       <>
         <Header />
+        <ToastContainer/>
         <Outlet />
         <Footer />
       </>
@@ -36,13 +39,13 @@ function App() {
     return (
       <>
         <div className="bg-[#151515] h-screen w-full flex flex-col justify-center items-center">
-          <div class="animate-pulse flex flex-col items-center gap-4 w-full h-full justify-center">
-            <div class="w-full h-[20%] bg-slate-400 rounded-md"></div>
-            <div class="w-[80%] h-[10%] bg-slate-400 mx-auto mt-3 rounded-md"></div>
-            <div class="h-[5%] bg-slate-400 w-[70%] rounded-md"></div>
-            <div class="h-[50%] bg-slate-400 w-[60%] rounded-md"></div>
-            <div class="h-[15%] bg-slate-400 w-[95%] rounded-md"></div>
-            <div class="h-[20%] bg-slate-400 w-full rounded-md"></div>
+          <div className="animate-pulse flex flex-col items-center gap-4 w-full h-full justify-center">
+            <div className="w-full h-[20%] bg-slate-400 rounded-md"></div>
+            <div className="w-[80%] h-[10%] bg-slate-400 mx-auto mt-3 rounded-md"></div>
+            <div className="h-[5%] bg-slate-400 w-[70%] rounded-md"></div>
+            <div className="h-[50%] bg-slate-400 w-[60%] rounded-md"></div>
+            <div className="h-[15%] bg-slate-400 w-[95%] rounded-md"></div>
+            <div className="h-[20%] bg-slate-400 w-full rounded-md"></div>
           </div>
         </div>
       </>
